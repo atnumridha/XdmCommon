c99f7d09c101f625a0fa9a6d408912a11b298d25
Atanu.Mridha <Atanu.Mridha@IN2271123W1.ey.net>
2018-05-06 12:57:38 +0530

    Changed Customisation Class

--- CommonCode/src/com/dmcsol/xdm/customization/OrgLayerCC.java
+++ CommonCode/src/com/dmcsol/xdm/customization/OrgLayerCC.java
@@ -1,26 +1,44 @@
 package com.dmcsol.xdm.customization;
 
+import oracle.adf.share.ADFContext;
+import oracle.adf.share.security.SecurityContext;
+
 import oracle.mds.core.MetadataObject;
 import oracle.mds.core.RestrictedSession;
 import oracle.mds.cust.CacheHint;
 import oracle.mds.cust.CustomizationClass;
 
 public class OrgLayerCC extends CustomizationClass {
+
+    private static final String DEFAULT_LAYER_NAME = "qrstravel";
+    private String mLayerName = DEFAULT_LAYER_NAME;
+    private String mLayerValue = null;
+
     @Override
     public CacheHint getCacheHint() {
         // TODO Implement this method
-        return CacheHint.ALL_USERS;
+        return CacheHint.USER;
     }
 
     @Override
     public String getName() {
         // TODO Implement this method
-        return "site";
+        return mLayerName;
     }
+
 
     @Override
     public String[] getValue(RestrictedSession restrictedSession, MetadataObject metadataObject) {
         // TODO Implement this method
-        return super.getValue(restrictedSession, metadataObject);
+        return new String[] { getMLayerValue() };
+    }
+
+    public String getMLayerValue() {
+        mLayerValue = ADFContext.getCurrent()
+                                .getSecurityContext()
+                                .getUserPrincipal()
+                                .getName();
+
+        return mLayerValue;
     }
 }
